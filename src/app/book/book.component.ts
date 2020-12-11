cc5b51ee0f4ae3a9df54b0a2e2ad80f0b17dc8a0
Martin Nebeling <martneb@aol.com>
2020-12-11 18:31:18 +0100

    Resolved #1

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from "../shared/api.service";
+import {Book} from "../model/book";
 
 @Component({
   selector: 'app-book',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookComponent implements OnInit {
 
-  constructor() { }
+  //create an array of books
+  books: Book[] = [];
+
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.getAllBooks();
   }
 
+  //create method getAllBooks() that makes use of apiService and on a successful response saves the books into an array
+  getAllBooks(){
+    this.apiService.getAllBooks().subscribe(
+      res => {
+        //write what you do with the response
+        this.books = res;
+      },
+      err => {
+        //write what you do in case of an error
+        alert("Something went wrong while getting books")
+      }
+    );
+  }
 }
