35e3a27572614e532422792176f20c8f29fdae2d
Martin Nebeling <martneb@aol.com>
2021-01-08 19:20:08 +0100

    added Router (Navigation through different pages)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { BookComponent } from './book/book.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {BookComponent} from './book/book.component';
 import {HttpClientModule} from "@angular/common/http";
-import { AuthorComponent } from './author/author.component';
+import {AuthorComponent} from './author/author.component';
+import {Route, RouterModule, Routes} from "@angular/router";
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'books',
+    component: BookComponent
+  },
+  {
+    path: 'authors',
+    component: AuthorComponent
+  },
+  {
+    path:'',
+    component:HomeComponent,
+    pathMatch:'full'
+  }
+
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     BookComponent,
-    AuthorComponent
+    AuthorComponent,
+    HomeComponent
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
