d970a02aa3e630930d4a91cb9a5804d47225f3a0
vitaliy <vkovalevskiy@omilia.com>
2021-02-22 14:22:10 +0200

    ts lint fix

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {ApiService} from "../shared/api.service";
-import {Book} from "../model/book";
-import {Author} from "../model/author";
+import {ApiService} from '../shared/api.service';
+import {Book} from '../model/book';
+import {Author} from '../model/author';
 
 @Component({
   selector: 'app-book',
@@ -10,71 +10,71 @@ import {Author} from "../model/author";
 })
 export class BookComponent implements OnInit {
 
-  //THIS SECTION IS FoR VARIABLES
-  //create an array of books
+  // THIS SECTION IS FoR VARIABLES
+  // create an array of books
   authors: Author[] = [];
   books: Book[] = [];
   book: Book = {
-    uuid: "",
-    name: "",
-    description: "",
-    releaseDate: "",
-    authorID: "",
-    quantity: "",
-    pages: ""
-  }
+    uuid: '',
+    name: '',
+    description: '',
+    releaseDate: '',
+    authorID: '',
+    quantity: '',
+    pages: ''
+  };
 
-  //THIS SECTION IS FOR CONSTRUCTOR
+  // THIS SECTION IS FOR CONSTRUCTOR
   constructor(private apiService: ApiService) {
   }
 
-  //THIS SECTION IS FOR METHODS (PRIORITY IMPLEMENTATION METHODS)
+  // THIS SECTION IS FOR METHODS (PRIORITY IMPLEMENTATION METHODS)
   ngOnInit(): void {
     this.getAllBooks();
     this.getAuthors();
   }
 
-  //create method getAllBooks() that makes use of apiService and on a successful response saves the books into an array
-  getAllBooks() {
+  // create method getAllBooks() that makes use of apiService and on a successful response saves the books into an array
+  getAllBooks(): void {
     this.apiService.getAllBooks().subscribe(
       res => {
-        //write what you do with the response
+        // write what you do with the response
         this.books = res;
       },
       err => {
-        //write what you do in case of an error
-        alert("Something went wrong while getting books")
+        // write what you do in case of an error
+        alert('Something went wrong while getting books');
       }
     );
   }
 
-  getAuthors(){
+  getAuthors(): void {
     this.apiService.getAllAuthors().subscribe(
-      res =>{
+      res => {
         this.authors = res;
       },
       error => {
-        alert("Couldn't find authors")
+        alert('Couldn\'t find authors');
       }
-    )
+    );
   }
 
-  saveBook() {
+  saveBook(): void {
     this.apiService.saveBook(this.book).subscribe(
-      //Recieves book without UUID, returns Book with UUID
+      // Recieves book without UUID, returns Book with UUID
       res => {
         this.books.push(res);
-        this.book.name = "";
-        this.book.description = "";
-        this.book.releaseDate = "";
-        this.book.pages = "";
-        this.book.quantity = "";
-        this.book.authorID = "";
+        this.book.name = '';
+        this.book.description = '';
+        this.book.releaseDate = '';
+        this.book.pages = '';
+        this.book.quantity = '';
+        this.book.authorID = '';
       },
       err => {
-        alert("Book couldn't be saved")
+        alert('Book couldn\'t be saved');
       }
-    )
+    );
 
   }
 }
